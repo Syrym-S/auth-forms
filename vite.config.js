62cc8dbc6261c988c467372f9dbdb680f240f576
Gani <ganiv847@gmail.com>
2026-06-09 09:31:43 +0500

    feat: ftp deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "src/main.jsx",
+      formats: ["iife"],
+      name: "ForwarderApp",
+      fileName: () => "index.js",
+      cssFileName: "index",
+    },
+  },
+});
