d552f69f4f76654d2444135771c91a4e5dab7315
Gani <ganiv847@gmail.com>
2026-07-14 13:12:26 +0300

    feat: api usage for customers info

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,8 @@
 // src/api/auth.js
-import { api } from './client';
+import { api } from "./client";
 
-export const loginRequest = (data) =>
-  api.post('/auth/v1/auth', data);
+export const loginRequest = (data) => api.post("/auth/v1/auth", data);
 
-export const registerRequest = (data) =>
-  api.post('/auth/v1/register', data);
+export const registerRequest = (data) => api.post("/auth/v1/register", data);
+
+export const getClaimInfoApi = () => api.get("/auth/v1/claim-info");
